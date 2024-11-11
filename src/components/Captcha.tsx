a7a95f6a21d979d8bad0516f3056b61ceda90765
Saahil <neon@saahild.com>
2024-11-10 22:43:30 -0500

    feat: captcha + main page

---
+++ src/components/Captcha.tsx
@@ -0,0 +1,6 @@
+import HCaptcha from '@hcaptcha/react-hcaptcha';
+export default function Captcha({ onVerify }: { onVerify: (token: string) => void }) {
+    return (
+        <HCaptcha sitekey={process.env.REACT_APP_HCAPTCHA_SITE_KEY!} onVerify={onVerify} />
+    )
+}
